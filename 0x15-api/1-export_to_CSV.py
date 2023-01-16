c715be32689194c55adeae3aac6c382a07e40e62
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2023-01-16 13:10:21 +0100

    task: Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This file makes a request to a url
+"""
+
+
+base_url = 'https://jsonplaceholder.typicode.com'
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+    import csv
+
+    if len(argv) > 1:
+        uid = argv[1]
+        r = requests.get('{}/{}/{}'.format(base_url, 'users', uid))
+
+        user = r.json()
+        r = requests.get('{}/{}?userId={}'.format(
+            base_url, 'todos', uid))
+        todos = r.json()
+        header_format = 'Employee {} is done with task({}/{}):'
+        c_todos = sum(todo['completed'] for todo in todos)
+        with open('{}.csv'.format(uid), 'w') as file:
+            writer = csv.writer(file)
+            for todo in todos:
+                data = [uid, user['username'],
+                        todo['completed'], todo['title']]
+                writer.writerow(data)
